b2c2cc5657501475a0e796ee5bc0a4e59f86cb44
Prima20 <primakurnia71@gmail.com>
2018-07-06 04:58:15 +0700

    Init commit
    Score. Obj pooling, Gamover cond not done yet

---
+++ Endless-example/Assets/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float m_MoveSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0f, 0f, m_MoveSpeed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Lose");
+        }
+    }
+}
