a9227f5f1c9bd9fba6cb8c9bc150ed79f832e18e
Prima20 <primakurnia71@gmail.com>
2018-08-04 05:56:34 +0700

    change name variable

--- Endless-example/Assets/Script/EnemyMovement.cs
+++ Endless-example/Assets/Script/EnemyMovement.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
-    public float moveSpeed;
+    public float speed;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0f, 0f, moveSpeed * Time.deltaTime);
+        transform.Translate(0f, 0f, -speed * Time.deltaTime);
 
         if (transform.position.z > 5)
         {
